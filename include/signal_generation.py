1758c78b8cf91c92ae518e613aaf0b9f1c4f7a96
sidikone <aboubakone@gmail.com>
2020-11-06 12:56:00 +0100

    uniq signal generator

--- include/signal_generation.py
+++ include/signal_generation.py
@@ -2,19 +2,87 @@ import matplotlib.pyplot as plt
 import numpy as np
 import math as m
 
-freq_ech = 100
-freq_sig = 5
-ampl = 8
 
-p_ech = 1./freq_ech
-signal_start = 5
-sign_end = 10
+class signalGenerator:
 
-time = np.arange(signal_start, sign_end+p_ech, p_ech)
-y=ampl*np.sin(2*np.pi*freq_sig*time)
-#print (time)
-#print(y)
+    def __init__(self, sampling_freq=100, signal_freq=1, signal_ampl=1):
+        self._sampling_freq = sampling_freq
+        self._signal_freq = signal_freq
+        self._signal_ampl = signal_ampl
 
-fig, ax = plt.subplots()
-ax.plot(time,y,'-o')
-plt.show()
+        self._signal_start = 0
+        self._signal_end = 1
+
+        self._signal_generation()
+
+    def _signal_generation(self) -> object:
+        self._sampling_period = 1. / self._sampling_freq
+        self._time = np.arange(self._signal_start, self._signal_end + self._sampling_period, self._sampling_period)
+        self._signal = self._signal_ampl * np.sin(2 * np.pi * self._signal_freq * self._time)
+
+        return None
+
+    def plot_signal(self, show=False):
+        self._signal_generation()
+        if show:
+            fig, ax = plt.subplots()
+            ax.set_ylabel("Amplitude")
+            ax.set_xlabel("Time")
+            plt.plot(self._time, self._signal)
+            plt.show()
+        return None
+
+    # getter methods
+    def get_sample_freq(self):
+        return self._sampling_freq
+
+    def get_signal_freq(self):
+        return self._signal_freq
+
+    def get_signal_ampl(self):
+        return self._signal_ampl
+
+    def get_signal_length(self):
+        return self._signal_start, self._signal_end
+
+    def get_signal_data(self):
+        self._signal_generation()
+        return self._time, self._signal
+
+    # setter methods
+    def set_sample_freq(self, freq):
+        self._sampling_freq = freq
+
+    def set_signal_freq(self, freq):
+        self._signal_freq = freq
+
+    def set_signal_ampl(self, ampl):
+        self._signal_ampl = ampl
+
+    def set_signal_length(self, start=0, end=1):
+        self._signal_start = start
+        self._signal_end = end
+
+
+
+def main():
+    sign1 = signalGenerator()
+    sign2 = signalGenerator()
+
+    sign1.set_signal_freq(5)
+    sign2.set_signal_freq(2.5)
+    sign1.set_signal_ampl(2)
+    sign2.set_signal_ampl(8)
+
+    sign1_x, sign1_y = sign1.get_signal_data()
+    sign2_x, sign2_y = sign2.get_signal_data()
+
+    fig, ax = plt.subplots()
+    ax.plot(sign1_x, sign1_y)
+    ax.plot(sign2_x, sign2_y)
+    plt.show()
+
+if __name__ == "__main__":
+    main()
+
+
