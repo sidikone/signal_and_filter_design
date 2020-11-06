285f65706d8f65291b3cc2e52a49b73566c3398a
sidikone <aboubakone@gmail.com>
2020-11-06 19:28:59 +0100

    raw multiple frequencies signal genenration

--- include/signal_generation.py
+++ include/signal_generation.py
@@ -1,6 +1,5 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import math as m
 
 error1 = "end value must be greater than start value !"
 
@@ -18,17 +17,41 @@ class SignalGenerator:
         self._time = 0
         self._signal = 0
 
-        self.signal_generation()
+        self._multiple_gen_trig = False
+        self.single_signal_generation()
 
-    def signal_generation(self):
+    def _time_gen(self):
         self._sampling_period = 1. / self._sampling_freq
         self._time = np.arange(self._signal_start, self._signal_end + self._sampling_period, self._sampling_period)
+        return None
+
+    def _amplitude_gen(self):
         self._signal = self._signal_ampl * np.sin(2 * np.pi * self._signal_freq * self._time)
+        return None
+
+    def single_signal_generation(self):
+        self._time_gen()
+        self._amplitude_gen()
+        return None
+
+    def multiple_signal_generation(self, parameters_list=0):
+
+        first = True
+        self._multiple_gen_trig = True
+        for (freq, ampl) in parameters_list:
+            if first:
+                self._signal = ampl * np.sin(2 * np.pi * freq * self._time)
+                print("yeah")
+                first = False
+            else:
+                self._signal += ampl * np.sin(2 * np.pi * freq * self._time)
+            print(freq, "\t", ampl)
 
         return None
 
     def plot_signal(self, show=False):
-        self.signal_generation()
+        if not self._multiple_gen_trig:
+            self.single_signal_generation()
         fig, ax = plt.subplots()
         ax.set_ylabel("Amplitude")
         ax.set_xlabel("Time")
@@ -39,8 +62,8 @@ class SignalGenerator:
         return None
 
     def plot_multiple(self, other, show=False):
-        self.signal_generation()
-        other.signal_generation()
+        self.single_signal_generation()
+        other.single_signal_generation()
         other_time, other_signal = other.get_signal_data()
 
         fig, ax = plt.subplots()
@@ -68,53 +91,46 @@ class SignalGenerator:
         return self._signal_start, self._signal_end
 
     def get_signal_data(self):
-        self.signal_generation()
+        self.single_signal_generation()
         return self._time, self._signal
 
     # setter methods
     def set_sample_freq(self, freq):
         self._sampling_freq = freq
+        self._time_gen()
 
     def set_signal_freq(self, freq):
         self._signal_freq = freq
+        self._time_gen()
 
     def set_signal_ampl(self, ampl):
         self._signal_ampl = ampl
+        self._amplitude_gen()
 
     def set_signal_length(self, start=0, end=1):
         self._signal_start = start
         self._signal_end = end
-
         if self._signal_start > self._signal_end:
             raise ValueError(error1)
-
-        # try:
-        #     self._signal_start = start
-        #     self._signal_end = end
-        #
-        #     self._signal_start>self._signal_start
-        #
-        # except ValueError:
-        #     raise RuntimeError
-
-        # if start>=end:
-        #     raise ValueError("signal end must be greater than signal start")
+        self._time_gen()
 
 
 def main():
     sign1 = SignalGenerator()
-    sign2 = SignalGenerator()
-
     sign1.set_signal_freq(5)
+    sign1.multiple_signal_generation([(2, 5), (12, 6) ,(7, 10)])
+
+    sign2 = SignalGenerator()
     sign2.set_signal_freq(2.5)
     sign2.set_signal_length(start=2, end=5)
 
-    sign1.set_signal_ampl(10)
-    sign2.set_signal_ampl(2.5)
-
-    # sign1.plot_signal(False)
-    # sign2.plot_signal(False)
-    sign1.plot_multiple(sign2, True)
+    sign1.plot_signal(True)
+    # sign1.set_signal_ampl(10)
+    # sign2.set_signal_ampl(2.5)
+    #
+    # # sign1.plot_signal(False)
+    # # sign2.plot_signal(False)
+    # sign1.plot_multiple(sign2, True)
 
 
 if __name__ == "__main__":
