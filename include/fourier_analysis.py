09224873dfc86d190533c8af80e8517392e3e780
sidikone <aboubakone@gmail.com>
2020-11-09 17:39:48 +0100

    add signal analysis

--- include/fourier_analysis.py
+++ include/fourier_analysis.py
@@ -1,9 +1,31 @@
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 import scipy.signal as ss
 
 
+def get_sampling_info(data):
+    time_delta = compute_timedelta(data)
+    dt = np.median(time_delta)
+    fs = 1 / dt
+
+    return fs, dt
+
+
+def compute_timedelta(data):
+    """
+
+    :param data:
+    :return:
+    """
+
+    diff = data.index.to_series().diff()
+    time_delta = [time.total_seconds() for time in diff]
+
+    return time_delta[1:]
+
+
 class SpectralAnalysis:
 
-    def __init__(self, sampling_freq):
-        self._sampling_freq = sampling_freq
+    def __init__(self, other):
+        self.sampling_frequency = get_sampling_info(other)
